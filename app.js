b3ee3a28df7b10ca00c3faedf62d58abed0a7bd1
kr_piatkowski <kr.piatkowski@gmail.com>
2011-12-02 10:25:54 +0000

    Added more dojo, and started on exercise

--- app.js
+++ app.js
@@ -37,4 +37,45 @@ server.get('/dojo', function(req, res){
    res.render('dojo');
 });
 
+
+
+
+//REST
+var kittens = [
+    {name: 'Abbymini',   color: 'black',     age: 1, cuteness:2},
+    {name: 'Apple',      color: 'white',     age: 3, cuteness:3},
+    {name: 'Asterisk',   color: 'browish',   age: 2, cuteness:3},
+    {name: 'Atom',       color: 'red',       age: 3, cuteness:3},
+    {name: 'Baby',       color: 'white',     age: 1, cuteness:3},
+    {name: 'Barbie',     color: 'white',     age: 1, cuteness:3},
+    {name: 'Biscuit',    color: 'white',     age: 2, cuteness:3},
+    {name: 'Bitsy',      color: 'white',     age: 4, cuteness:3}
+];
+
+server.get('/kittens', function(req, res){
+    res.send(kittens);
+});
+
+server.get('/kittens/:kitten', function(req, res){
+    for(var i = 0; i < kittens.length; i++){
+        var kitten = kittens[i];
+        if(kitten.name === req.params.kitten){
+            res.send(kitten);
+            return;
+        }
+    }
+    return null;
+})
+
+server.del('/kittens/:kitten', function(req, res){
+    for(var i = 0; i < kittens.length; i++){
+        var kitten = kittens[i];
+        if(kitten.name === req.params.kitten){
+            delete kittens[i];
+            return;
+        }
+    }    
+});
+
+
 server.listen(process.env.PORT);
